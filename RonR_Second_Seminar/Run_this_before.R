b617f9a31c488555b05e24136839343a58020fac
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-15 19:04:01 +0200

    Update Run_this_before.R

--- RonR_Second_Seminar/Run_this_before.R
+++ RonR_Second_Seminar/Run_this_before.R
@@ -1,15 +1,21 @@
-## This code installs the second seminar course in this folder.
+## This code installs the second seminar course in your working directory.
+
+packages <- c("downloader", "swirl")
+
+if (length(setdiff(packages, installed.packages())) > 0) {
+    install.packages(setdiff(packages, installed.packages()))
+}
+
+library(swirl)
+library(downloader)
 
 ## Download the file
-download.file("https://github.com/cimentadaj/Rseminars/archive/master.zip",
-              "Rseminars-master.zip", method = "libcurl")
+download("https://github.com/cimentadaj/Rseminars/archive/master.zip",
+              "Rseminars-master.zip", mode="wb")
 
 ## unzip and install
 install_course_zip(paste0(getwd(),"/Rseminars-master.zip"),
                    multi=T, which_course = "RonR_Second_Seminar")
-
-install.packages("swirl")
-library(swirl)
 
 ## Now you can see the course if you type swirl()
 message("Type swirl() to access the course")
