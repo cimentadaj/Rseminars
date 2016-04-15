dac99a812488e4ac6200e570f0bb9e226f46f54b
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-15 19:22:14 +0200

    Delete Install_packages.R

--- RonR_Second_Seminar/Install_packages.R
+++
@@ -1,7 +0,0 @@
-## download all packages
-
-packages <- c("downloader", "swirl")
-
-if (length(setdiff(packages, installed.packages())) > 0) {
-    install.packages(setdiff(packages, installed.packages()))
-}
