d8ce01d166203d660f97dace490588e78b0edb87
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-15 19:39:24 +0200

    Create This_will_install_everything.R

---
+++ RonR_Second_Seminar/This_will_install_everything.R
@@ -0,0 +1,7 @@
+
+## install.packages("downloader") if you don't have it installed.
+
+library(downloader)
+
+source_url("https://github.com/cimentadaj/Rseminars/blob/master/RonR_Second_Seminar/Course_download.R",
+        "50955a21c480d5d5062e2ba8bf9ae6b83b8fd4fe")
