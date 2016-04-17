ff57056644a2f93bec0f0a99b54588e524cdb61d
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-17 21:23:50 +0200

    Update This_will_install_everything.R

--- RonR_Second_Seminar/This_will_install_everything.R
+++ RonR_Second_Seminar/This_will_install_everything.R
@@ -1,6 +1,10 @@
+## For this seminar there is not Powerpoint presentation. We decided to teach R within R. Instead,
+## we've programmed the lessons and exercises that will appear interactively in R.
+## Open your R console, preferably Rstudio, and run the code below.
+## You shall get instructions on how to start the session as soon as everything is installed..
+
 
 ## install.packages("downloader") if you don't have it installed.
-
 library(downloader)
 
 source_url("https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Second_Seminar/Course_download.R",
