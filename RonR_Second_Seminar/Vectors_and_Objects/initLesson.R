19227f431cfae179b7e00d67ddb687b01accb039
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-18 23:41:38 +0200

    Added files via upload

---
+++ RonR_Second_Seminar/Vectors_and_Objects/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+income <- c(10, 5, 3, 6, 21)
+diabetes <- c("Type1","Type2","Type1","Type1")
+status <- c("Poor","Improved","Excellent")
+status2 <- factor(status, order = TRUE, levels=c("Poor", "Improved", "Excellent"))
