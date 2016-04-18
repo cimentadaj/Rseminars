c3ab53388aacfed5a0557b45736a7c75f7f09a6e
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-19 00:39:21 +0200

    Added files via upload

---
+++ RonR_Second_Seminar/Data_frames_I/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+states <- data.frame(code= c("CA", "NY", "NE", "AZ"), 
+                     population = c(38.8, 19.7, 2.1, 6.8), 
+                     region     = c("West", "Northeast", "Midwest", "West"), 
+                     landlock   = c(FALSE, FALSE, TRUE, TRUE))
