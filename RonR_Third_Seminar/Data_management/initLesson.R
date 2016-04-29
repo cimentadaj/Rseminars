fbe4e9982c7d8ae5a179b8a73d7b4c6142500b5d
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-29 18:07:43 +0200

    Added files via upload

---
+++ RonR_Third_Seminar/Data_management/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+asfr7512 <- read.table("asfr752012ine1.csv", sep=",", header = TRUE)
+asfr1314 <- read.table("asfr1314ine.txt", sep="",header= TRUE)
+
+even <- seq(2,40,2)
