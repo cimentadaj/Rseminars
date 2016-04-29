1c122b7ec35303300460689e1572fb199d80ff23
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-29 19:30:19 +0200

    Create This_installs_everything.R

---
+++ RonR_Third_Seminar/This_installs_everything.R
@@ -0,0 +1,9 @@
+
+if ("downloader" %in% installed.packages() == F) {
+    install.packages("downloader")
+}
+
+library(downloader)
+
+source_url("https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/Installation.R",
+paste(sha_url("https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/Installation.R")))
