ae004048b1bda645128f4bb2c5675effb4799646
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-29 16:08:48 +0200

    Create Installation.R

---
+++ RonR_Third_Seminar/Installation.R
@@ -0,0 +1,26 @@
+library(downloader)
+library(swirlify)
+
+dir.create(paste0(getwd(), "/Third_R_Seminar"))
+setwd(paste0(getwd(), "/Third_R_Seminar"))
+
+## Download the file
+download.file("https://github.com/cimentadaj/Rseminars/archive/master.zip",
+              paste0(getwd(),"/Rseminars-master.zip"), method="curl")
+
+## unzip and install
+
+files <- c("Rseminars-master/RonR_Third_Seminar/Dataset.zip")
+unzip(paste0(getwd(), "/Rseminars-master.zip"), exdir=getwd(), files = files)
+
+my.file.rename <- function(from, to) {
+    todir <- dirname(to)
+    if (!isTRUE(file.info(todir)$isdir)) dir.create(todir, recursive=TRUE)
+    file.rename(from = from,  to = to)
+}
+
+my.file.rename(paste0(getwd(),"/Rseminars-master/RonR_Third_Seminar/Dataset.zip"), paste0(getwd(), "/Dataset.zip"))
+
+unzip(paste0(getwd(),"/Dataset.zip"), exdir=getwd())
+
+install_course_zip(paste0(getwd(),"/Rseminars-master.zip"), multi=T, which_course = "RonR_Third_Seminar")
