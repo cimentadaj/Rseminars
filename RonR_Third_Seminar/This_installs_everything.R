b21de2d8aeb493d9c0a0b6db3e9d8630929e48a9
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-30 12:03:29 +0200

    Update This_installs_everything.R

--- RonR_Third_Seminar/This_installs_everything.R
+++ RonR_Third_Seminar/This_installs_everything.R
@@ -1,4 +1,8 @@
-## Run this code and you should get all the lessons of the third RonR! seminar installed.
+## For this seminar there is no Powerpoint presentation. We decided to teach R within R. Instead,
+## we've programmed the lessons and exercises to appear interactively in R.
+## Open your R console, preferably Rstudio, and run the code below.
+## You shall get instructions on how to start the 3rd session as soon as everything is installed.
+
 if ("downloader" %in% installed.packages() == F) {
     install.packages("downloader")
 }
@@ -7,3 +11,6 @@ library(downloader)
 
 source_url("https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/Installation.R",
 paste(sha_url("https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/Installation.R")))
+
+# If you're using windows replace the curl with "libcurl". If you're using Mac OS leave the curl as it is.
+install_everything("libcurl")
