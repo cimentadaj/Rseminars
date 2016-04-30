7903be74cd5a1aa984cbd20d7a162eeb4d76fcc1
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-30 12:10:09 +0200

    Update This_will_install_everything.R

--- RonR_Third_Seminar/This_will_install_everything.R
+++ RonR_Third_Seminar/This_will_install_everything.R
@@ -1,3 +1,7 @@
+## For this seminar there is no Powerpoint presentation. We decided to teach R within R. Instead,
+## we've programmed the lessons and exercises to appear interactively in R.
+## Open your R console, preferably Rstudio, and run the code below.
+## You shall get instructions on how to start the session as soon as everything is installed.
 
 
 if ("downloader" %in% installed.packages()) == F {
