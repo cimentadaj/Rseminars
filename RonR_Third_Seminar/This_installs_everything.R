8e232030adb3ff233c37abe0197f0d20edfb1ec4
Jorge Cimentada <cimentadaj@gmail.com>
2016-04-30 12:03:53 +0200

    Update This_installs_everything.R

--- RonR_Third_Seminar/This_installs_everything.R
+++ RonR_Third_Seminar/This_installs_everything.R
@@ -12,5 +12,5 @@ library(downloader)
 source_url("https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/Installation.R",
 paste(sha_url("https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/Installation.R")))
 
-# If you're using windows replace the curl with "libcurl". If you're using Mac OS leave the curl as it is.
+## If you're using windows replace the curl with "libcurl". If you're using Mac OS leave the curl as it is.
 install_everything("libcurl")
