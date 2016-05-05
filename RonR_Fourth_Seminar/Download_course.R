1d42eeabe93a557cc8cdda44b3bf0c51cf541868
Jorge Cimentada <cimentadaj@gmail.com>
2016-05-05 09:07:03 +0200

    Update Download_course.R

--- RonR_Fourth_Seminar/Download_course.R
+++ RonR_Fourth_Seminar/Download_course.R
@@ -17,7 +17,7 @@ download.file("https://github.com/cimentadaj/Rseminars/archive/master.zip",
 
 ## unzip and install
 install_course_zip(paste0(getwd(),"/Rseminars-master.zip"),
-                   multi=T, which_course = "RonR_Second_Seminar")
+                   multi=T, which_course = "RonR_Fourth_Seminar")
 
 ## Now you can see the course if you type swirl()
 message("Type swirl() to access the course")
