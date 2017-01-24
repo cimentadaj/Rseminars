6856d668a0a2cdea8a51d7919e467802549e07a5
cimentadaj <cimentadaj@gmail.com>
2017-01-24 17:31:43 +0100

    Correcting second lesson

--- RonR_Second_Seminar/Vectors_and_Objects/initLesson.R
+++ RonR_Second_Seminar/Vectors_and_Objects/initLesson.R
@@ -3,7 +3,7 @@
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
 
-income <- c(10, 5, 3, 6, 21)
+income <- c(100, 500, 35, 60, 2100)
 diabetes <- c("Type1","Type2","Type1","Type1")
 status <- c("Poor","Improved","Excellent")
 status2 <- factor(status, order = TRUE, levels=c("Poor", "Improved", "Excellent"))
