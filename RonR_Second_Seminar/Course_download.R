e1ca32e07d39bc8138d4004281170e8189736ceb
cimentadaj <cimentadaj@gmail.com>
2017-04-07 10:42:49 +0200

    Fixed download fun - no warnings

--- RonR_Second_Seminar/Course_download.R
+++ RonR_Second_Seminar/Course_download.R
@@ -1,14 +1,14 @@
 ## This function installs the swirl course.
 
 install_everything <- function() {
-packages <- "swirl"
-packages_to_install <- setdiff(packages, installed.packages())
-
-if (length(packages_to_install) > 0) install.packages(packages_to_install)
-require(swirl)
-
-install_course_github("cimentadaj", "Rseminars", multi = T)
-
-## Now you can see the course if you type swirl()
-message("Type swirl() to access the course")
+  packages <- "swirl"
+  packages_to_install <- setdiff(packages, installed.packages())
+  
+  if (length(packages_to_install) > 0) install.packages(packages_to_install)
+  suppressWarnings(suppressPackageStartupMessages(require(swirl)))
+  
+  suppressWarnings(install_course_github("cimentadaj", "Rseminars", multi = T))
+  
+  ## Now you can see the course if you type swirl()
+  message("Type swirl() to access the course")
 }
