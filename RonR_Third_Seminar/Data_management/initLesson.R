ded09c0f581876806a646b8cf38be6f1680de086
cimentadaj <cimentadaj@gmail.com>
2017-05-04 08:25:58 +0200

    Completed about 30% and added datasets

--- RonR_Third_Seminar/Data_management/initLesson.R
+++ RonR_Third_Seminar/Data_management/initLesson.R
@@ -3,7 +3,7 @@
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
 
-asfr7512 <- read.table("asfr752012ine1.csv", sep=",", header = TRUE)
-asfr1314 <- read.table("asfr1314ine.txt", sep="",header= TRUE)
+asfr7512 <- read.table("./data/asfr752012ine1.csv", sep=",", header = TRUE)
+asfr1314 <- read.table("./data/asfr1314ine.txt", sep="", header= TRUE)
 
 even <- seq(2,40,2)
