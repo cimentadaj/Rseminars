4853d55a0ebe78458eb5590f09cb63e5adfbdc2f
cimentadaj <cimentadaj@gmail.com>
2017-05-09 08:16:43 +0200

    Started descriptive lesson and fixed about 4-5 questions

--- RonR_Third_Seminar/Descriptive_statistics/initLesson.R
+++ RonR_Third_Seminar/Descriptive_statistics/initLesson.R
@@ -2,3 +2,5 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
+
+txt_file <- "https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/Data_management/data/asfr1314ine.txt"
