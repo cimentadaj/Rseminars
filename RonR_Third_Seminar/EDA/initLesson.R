31ed5881d529b1a69ec835988d6b77ae9c6ecb37
cimentadaj <cimentadaj@gmail.com>
2017-05-24 08:14:50 +0200

    Read both datasets correctly in EDA

--- RonR_Third_Seminar/EDA/initLesson.R
+++ RonR_Third_Seminar/EDA/initLesson.R
@@ -2,3 +2,8 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+
+data_paths <- file.path(find.package("swirl"), "Courses", "RonR_Third_Seminar", "EDA", "data")
+
+wvs_url <- "https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/EDA/data/wvs"
+wvs2_url <- "https://raw.githubusercontent.com/cimentadaj/Rseminars/master/RonR_Third_Seminar/EDA/data/wvs_semi_colon"
